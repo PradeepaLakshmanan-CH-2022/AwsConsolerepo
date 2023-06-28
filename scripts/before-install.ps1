074859b8e8e000c19539bd589cbc2bdc09d98217
Pradee67 <109140915+Pradee67@users.noreply.github.com>
2023-06-28 17:19:48 +0530

    ps

--- scripts/before-install.ps1
+++ scripts/before-install.ps1
@@ -1,5 +1,9 @@
-# Import-Module -Name ServerManager
-# Install-WindowsFeature Web-Server
+# Import the ServerManager module
+Import-Module -Name ServerManager
+
+# Install the Web-Server feature
+Install-WindowsFeature -Name Web-Server
+
 # Change to the directory where your API application is located
 Set-Location -Path "C:\MyCode\DockerCode\PipelineTesting"
 
