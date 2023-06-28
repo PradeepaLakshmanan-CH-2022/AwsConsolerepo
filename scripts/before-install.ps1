4b5df4957e10232c9387235812e5e0bbe69a720e
Pradee67 <109140915+Pradee67@users.noreply.github.com>
2023-06-28 17:50:56 +0530

    changed file

--- scripts/before-install.ps1
+++ scripts/before-install.ps1
@@ -5,7 +5,7 @@ Import-Module -Name ServerManager
 Install-WindowsFeature -Name Web-Server
 
 # Change to the directory where your API application is located
-Set-Location -Path "C:\MyCode\DockerCode\PipelineTesting"
+# Set-Location -Path "C:\MyCode\DockerCode\PipelineTesting"
 
-# Run the .NET API application
-Start-Process -FilePath "dotnet" -ArgumentList "PipelineTesting.dll" -NoNewWindow
+# # Run the .NET API application
+# Start-Process -FilePath "dotnet" -ArgumentList "PipelineTesting.dll" -NoNewWindow
