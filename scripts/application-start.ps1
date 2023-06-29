52fccdb40c2902f010f806c7f4ab4dc439d8c880
Pradee67 <109140915+Pradee67@users.noreply.github.com>
2023-06-29 11:18:00 +0530

    appspec

--- scripts/application-start.ps1
+++ scripts/application-start.ps1
@@ -1,7 +1,7 @@
 # Example script for application-start hook
 
 # Change to the directory where your .NET API application is located
-Set-Location -Path "C:\MyCode\MyApiApplication"
+Set-Location -Path "C:\MyCicdApplication"
 
 # Run the .NET API application
 Start-Process -FilePath "dotnet" -ArgumentList "run" -NoNewWindow
