a6c2e68330529804ea0027eb646f659c9ba9221c
Pradee67 <109140915+Pradee67@users.noreply.github.com>
2023-06-30 13:11:33 +0530

    new scripts

--- scripts/start_api.ps1
+++ scripts/start_api.ps1
@@ -1,5 +1,17 @@
-# Change directory to the application folder
-Set-Location -Path C:\inetpub\wwwroot\YourAppFolder
+# Set the application path
+$applicationPath = "C:\PipelineDeployment\ApiPipelineDeploy\PipelineTesting"
 
-# Start the .NET API application
-Start-Process -NoNewWindow -FilePath "dotnet" -ArgumentList "PipelineTesting.dll"
+# Clean the application directory
+Remove-Item -Path $applicationPath\* -Recurse -Force
+
+# Build and publish the application
+dotnet publish -c Release -o $applicationPath
+
+# Create a new IIS application pool
+New-WebAppPool -Name MyApiAppPool
+
+# Create a new IIS website
+New-Website -Name MyApiWebsite -PhysicalPath $applicationPath -ApplicationPool MyApiAppPool -Port 80
+
+# Start the IIS website
+Start-Website -Name MyApiWebsite
