160789cfdf79c55b885dbfd68a9927355d75c243
Pradee67 <109140915+Pradee67@users.noreply.github.com>
2023-06-30 10:57:50 +0530

    app scripts

--- scripts/before-install.ps1
+++ scripts/before-install.ps1
@@ -1,18 +1,3 @@
-# # Import the ServerManager module
-# Import-Module -Name ServerManager
-
-# # Install the Web-Server feature
-# Install-WindowsFeature -Name Web-Server
-
-# # Change to the directory where your API application is located
-# # Set-Location -Path "C:\MyCode\DockerCode\PipelineTesting"
-
-# # # Run the .NET API application
-# # Start-Process -FilePath "dotnet" -ArgumentList "PipelineTesting.dll" -NoNewWindow
-# Stop the .NET API application
-# Stop-Process -Name "dotnet" -Force
-# Example script for before-install hook
-
-# Stop the default IIS application pool
-Stop-WebAppPool -Name "DefaultAppPool"
-
+ 
+ Import-Module -Name ServerManager
+ Install-WindowsFeature Web-Server
